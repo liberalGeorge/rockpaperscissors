e1886d7a465007ab7f0b90ff42edd2151337afe2
Georgina Hughes <me@georginahughes.com>
2016-06-12 15:41:57 +0100

    Human functionality logic, Update to HTML

--- src/js/module.js
+++ src/js/module.js
@@ -3,12 +3,44 @@ const ui = {
 		humanPlayerBoard: document.getElementById('humanGameBoard'),
 		playerOneBoard: document.getElementById('gameBoard1'),
 		playerTwoBoard: document.getElementById('gameBoard2'),
-		humanBoardOptions: document.getElementById('humanOptions')
+		human: {
+			gameArea: document.getElementById('humanGameBoard'),
+			optionsArea: document.getElementById('humanOptions'),
+			choiceDisplayArea: document.getElementById('humanChoice'),
+			options: {
+				rock: document.getElementById('rock'),
+				paper: document.getElementById('paper'),
+				scissors: document.getElementById('scissors')
+			}
+		},
+		playerOne: {
+			gameArea: document.getElementById('gameBoard1'),
+			choiceDisplayArea: document.getElementById('playerOneChoice')
+		},
+		playerTwo: {
+			gameArea: document.getElementById('gameBoard2'),
+			choiceDisplayArea: document.getElementById('playerTwoChoice')
+		}
 	},
+
 	values = {
 		human: 'human',
-		computer: 'computer'
+		computer: 'computer',
+		rock: {
+			name: 'rock',
+			id: 1
+		},
+		paper: {
+			name: 'paper',
+			id: 2
+		},
+		scissors: {
+			name: 'scissors',
+			id: 3
+		}
 	};
+
+let firstPlayersChoice = '';
 
 export default function initHandlers(){
 	ui.gameModeSelect.onchange = setGameMode;
@@ -27,18 +59,59 @@ function startGame(mode){
 			break;
 		case values.computer:
 			setPlayerOnesTurn();
+
+			//put delay in here
+
+			setPlayerTwosTurn();
 			break;
 		default:
 			break;
 	}
 
-	//put delay in here
-
-	setPlayerTwosTurn();
+	//disable drop down here
 }
 
 function getHumanPlayersTurn(){
+	initHumanEventHandlers();
+	showHumanGameArea();
+}
 
+function showHumanGameArea(){
+	ui.human.gameArea.style.display = 'block';
+}
+
+function initHumanEventHandlers(){
+	ui.human.options.rock.onclick = setHumanSelection;
+	ui.human.options.paper.onclick = setHumanSelection;
+	ui.human.options.scissors.onclick = setHumanSelection;
+}
+
+function setHumanSelection(e){
+	switch(e.target.id){
+		case values.rock.name:
+			firstPlayersChoice = values.rock.id;
+			break;
+		case values.paper.name:
+			firstPlayersChoice = values.paper.id;
+			break;
+		case values.scissors.name:
+			firstPlayersChoice = values.scissors.id;
+			break;
+		default:
+			break;
+	}
+
+	console.log(firstPlayersChoice);
+
+	humansChoiceMade();
+}
+
+function humansChoiceMade(){
+	ui.human.optionsArea.style.display = 'none';
+
+	//put delay here
+
+	setPlayerTwosTurn();
 }
 
 function setPlayerOnesTurn(){}
